b38048bcaac0d551b213869aa9cf2ee9fa8d1d88
Reed Wigley <rwigley55@gmail.com>
2024-05-06 15:09:18 -0700

    add footer and styling

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 function ai_health_files()
 {
     wp_enqueue_script("navbar-dropdown", get_theme_file_uri("dropdown.js"), NULL, "1.0", true);
-    wp_enqueue_script("font-awesome", "https://kit.fontawesome.com/25e9a024ec.js", NULL, "1.0", true);
+    wp_enqueue_script("font-awesome", "https://kit.fontawesome.com/25e9a024ec.js", NULL, "6.5.2", true);
     // wp_enqueue_style("custom-google-fonts", get_theme_file_uri("//fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap"));
     wp_enqueue_style("health_main_styles", get_theme_file_uri("/css/index.css"));
 }
