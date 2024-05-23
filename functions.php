e08ec735204c231631252a778b8c2eb546d347a7
Reed Wigley <rwigley55@gmail.com>
2024-05-23 14:53:54 -0700

    single-device page styling

--- functions.php
+++ functions.php
@@ -14,6 +14,7 @@ add_action("wp_enqueue_scripts", "ai_health_files");
 function theme_features()
 {
     add_theme_support("title-tag");
+    add_theme_support("post-thumbnails");
 }
 
 add_action("after_setup_theme", "theme_features");
--- single-device.php
+++ single-device.php
@@ -5,11 +5,13 @@ get_header();
 while (have_posts()) {
     the_post(); ?>
     <section>
-        <div class="blog__container">
-            <h1><?php the_title(); ?></h1>
-            <?php the_content();
-            the_field("device_price");
-            ?>
+        <div class="device__container">
+            <?php the_post_thumbnail(); ?>
+            <div class="device__detail-container">
+                <h1><?php the_title(); ?></h1>
+                <h2>$<?php the_field("device_price"); ?></h2>
+                <?php the_content(); ?>
+            </div>
         </div>
     </section>
 <?php }
